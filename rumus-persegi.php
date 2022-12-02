f7393885c49be223e8fc7ae7d25ace2f959136fe
Ibnu nasrulloh <110141514+IbnuZ@users.noreply.github.com>
2022-12-02 17:28:24 +0700

    Add files via upload

---
+++ rumus-persegi.php
@@ -0,0 +1,11 @@
+<form action="rumus-persegi.php" method="POST">
+    <h1> rumus luas keliling persegi </h1>
+    <p>Sisi :</p> 
+    <input type="number" name="Sisi" placeholder="Ex, 5" /><br>
+    <input type="subit" name="proses" value="hitung luas & keliling"/>
+</form>
+
+<?php
+        if ( issset($_POST["Proses"]) ) {
+            echo "<"
+        }
